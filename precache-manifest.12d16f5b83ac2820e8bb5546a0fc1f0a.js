4c7291341a0b8b9ccbd943d0849b447cb2c4c601
Sharon Kim <sharonkim1709@gmail.com>
2020-07-31 18:38:18 -0400

    Updates

---
+++ precache-manifest.12d16f5b83ac2820e8bb5546a0fc1f0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9282bd5ef670bef255cb0c60d02e7c2c",
+    "url": "/react-employee-app/index.html"
+  },
+  {
+    "revision": "b1cda2cb11a5af8d8697",
+    "url": "/react-employee-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79c1565afb154f2c7e7b",
+    "url": "/react-employee-app/static/js/2.4b7aa055.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-app/static/js/2.4b7aa055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1cda2cb11a5af8d8697",
+    "url": "/react-employee-app/static/js/main.b0de92a5.chunk.js"
+  },
+  {
+    "revision": "9536a1b8f2749b8275bf",
+    "url": "/react-employee-app/static/js/runtime-main.67c68914.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-employee-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
