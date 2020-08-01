6d48a3311d4761ad4f378cafb742b9d13a154155
Sharon Kim <sharonkim1709@gmail.com>
2020-08-01 11:19:32 -0400

    added form input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchResultContainer from "./components/SearchResultContainer"
+import SearchForm from "./components/SearchForm"
 function App() {
   return (
     <div className="App">
     <h1 className="text-center">Employee Directory</h1>
     <p className="text-center">Click on controls to filter by heading or use the search box to narrow your results.</p>
+    <SearchForm/>
     <SearchResultContainer/>
     </div>
   );
