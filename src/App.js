a2074c9d8bad68bcb7c1dac63e12c55f813da077
Sharon Kim <sharonkim1709@gmail.com>
2020-08-02 10:00:18 -0400

    successfully can sort first names of all employees

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
   state = {
     search: "",
     employees: [],
-    filteredEmployees:[]
+    filteredEmployees: [],
+    sortedName: true,
   };
 
   // When this component mounts, search the RandomUserGenerator API for pictures of kittens
@@ -17,47 +18,67 @@ class App extends Component {
     API.search()
       .then((res) => {
         console.log(res);
-        this.setState({ employees: res.data.results,
-        filteredEmployees:res.data.results
+        this.setState({
+          employees: res.data.results,
+          filteredEmployees: res.data.results,
         });
       })
       .catch((err) => {
         console.log(err);
       });
   }
-
-  handleInputChange = event => {
+// function to handle the change in input and looks for employees with what is in the input
+  handleInputChange = (event) => {
     event.preventDefault();
-    this.setState({ search: event.target.value })
+    this.setState({ search: event.target.value });
     console.log(event.target.value);
     const filter = event.target.value;
-    const filteredList = this.state.employees.filter(employee => {
-        // merge data together, then see if user input is anywhere inside
-        const { first: firstName, last: lastName } = employee.name;
-        const name = `${firstName} ${lastName}`;
-        return name.toLowerCase().includes(filter.toLowerCase().trim());
+    const filteredList = this.state.employees.filter((employee) => {
+      // merge data together, then see if user input is anywhere inside
+      const { first: firstName, last: lastName } = employee.name;
+      const name = `${firstName} ${lastName}`;
+      return name.toLowerCase().includes(filter.toLowerCase().trim());
     });
     this.setState({ filteredEmployees: filteredList });
-}
+  };
+// function to alphabetize first names of employees
+  sortName = () => {
+    let currentEmployees = this.state.employees;
+    if (this.state.sortedName=== true) {
+      currentEmployees.sort((a, b)=> {
+        if (a.name.first.toLowerCase()< b.name.first.toLowerCase()) return -1;
+        return 0;
+      });
+      this.setState({ employees: currentEmployees, sortedName: false });
+    } else {
+      currentEmployees.sort((a, b) =>{
+        if (a.name.first.toLowerCase() > b.name.first.toLowerCase()) return -1;
+        return 0;
+      });
+      this.setState({ employees: currentEmployees, sortedName: true });
+    }
+  };
 
   render() {
     return (
       <>
         <Heading />
-        <SearchForm 
-        value={this.state.search}
-       handleInputChange={this.handleInputChange}
-       />
+        <SearchForm
+          value={this.state.search}
+          handleInputChange={this.handleInputChange}
+          sortName={this.sortName}
+        />
         <ContentHead />
         {this.state.filteredEmployees.map((employee, id) => (
-          <EmployeeCard 
-          key={id}
-          image={employee.picture.thumbnail} 
-          firstName={employee.name.first}
-          lastName={employee.name.last}
-          phoneNumber={employee.phone}
-          email={employee.email}
-          dob={employee.dob.date}/>
+          <EmployeeCard
+            key={id}
+            image={employee.picture.thumbnail}
+            firstName={employee.name.first}
+            lastName={employee.name.last}
+            phoneNumber={employee.phone}
+            email={employee.email}
+            dob={employee.dob.date}
+          />
         ))}
       </>
     );
